a5795e125aaa6ef3bb98fd8ce2f3ca87f602c013
VU <vu@VUs-MacBook-Pro-2.local>
2025-02-27 22:57:21 +0700

    update About page

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,6 +3,10 @@ import meta from './meta.png'
 import shopify from './shopify.png'
 import starbucks from './starbucks.png'
 import tesla from './tesla.png'
+import ttv from './ttv.png'
+import freelancer from './freelancer.png'
+import mwg from './mwg.webp'
+import fujinet from './fujinet.svg'
 import logo from './logo.svg'
 
 export {
@@ -11,5 +15,9 @@ export {
     shopify,
     starbucks,
     tesla,
+    ttv,
+    freelancer,
+    mwg,
+    fujinet,
     logo
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,34 +2,34 @@ import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeli
 import 'react-vertical-timeline-component/style.min.css';
 
 import { skills, experiences } from '../constants';
-import CTA from '../components/CTA';
+import Cta from '../components/CTA';
 
 const About = () => {
     return (
         <section className="max-container">
             <h1 className="head-text">
-                Hello, I'm <span className='blue-gradient_text font-semibold drop-shadow'>BlackCat 13</span>
+                Hello, I'm <span className='font-semibold blue-gradient_text drop-shadow'>Mr.Vu</span>
             </h1>
 
-            <div className="mt-5 flex- flex-col gap-3 text-slate-500">
+            <div className="flex-col gap-3 mt-5 flex- text-slate-500">
                 <p> Software Engineer based in VietNam,
                     specializing in technical education through hands-on learning and building applications. </p>
             </div>
 
-            <div className="py-10 flex flex-col">
+            <div className="flex flex-col py-10">
                 <h3 className="subhead-text">
                     My Skills
                 </h3>
 
-                <div className="mt-16 flex flex-wrap gap-12">
-                    {skills.map((skill) => (
-                        <div className='block-container w-20 h-20'>
+                <div className="flex flex-wrap gap-12 mt-16">
+                    {skills.map((skill, index) => (
+                        <div key={skill.name} className='w-20 h-20 block-container'>
                             <div className='btn-back rounded-xl' />
-                            <div className='btn-front rounded-xl flex justify-center items-center'>
+                            <div className='flex items-center justify-center btn-front rounded-xl'>
                                 <img
                                     src={skill.imageUrl}
                                     alt={skill.name}
-                                    className='w-1/2 h-1/2 object-contain'
+                                    className='object-contain w-1/2 h-1/2'
                                 />
                             </div>
                         </div>
@@ -42,20 +42,21 @@ const About = () => {
                     Work Experience
                 </h3>
 
-                <div className="mt-5 flex- flex-col gap-3 text-slate-500">
-                    <p> I've work with all sorts of companies,
-                        leveling up my skills and teaming up with smart people.
-                        Here's the rundown: </p>
+                <div className="flex-col gap-3 mt-5 flex- text-slate-500">
+                    <p> I have 7 years of experience as a Full-Stack JS Developer.
+                        During these 7 years, I had the chance to work with ReactJs and NextJs for approximately 5 years, and 3 years with React Native.
+                        Recently, I have had 2 years with NodeJs and AWS and a half year with Flutter .
+                    </p>
                 </div>
 
-                <div className='mt-12 flex'>
+                <div className='flex mt-12'>
                     <VerticalTimeline>
                         {experiences.map((experience) => (
                             <VerticalTimelineElement
                                 key={experience.company_name}
                                 date={experience.date}
                                 icon={
-                                    <div className='flex justify-center items-center w-full h-full'>
+                                    <div className='flex items-center justify-center w-full h-full'>
                                         <img
                                             src={experience.icon}
                                             alt={experience.company_name}
@@ -74,17 +75,17 @@ const About = () => {
                                 }}
                             >
                                 <div>
-                                    <h3 className='text-black text-xl font-poppins font-semibold'>
+                                    <h3 className='text-xl font-semibold text-black font-poppins'>
                                         {experience.title}
                                     </h3>
-                                    <p className='text-black-500 font-medium font-base' style={{ margin: 0 }}>
+                                    <p className='font-medium text-black-500 font-base' style={{ margin: 0 }}>
                                         {experience.company_name}
                                     </p>
                                 </div>
-                                <ul className='my-5 list-disc ml-5 space-y-2'>
+                                <ul className='my-5 ml-5 space-y-2 list-disc'>
                                     {experience.points.map((point, index) => (
-                                        <li key={`experience-point-${index}`}
-                                            className='text-black-500/50 font-normal pl-1 text-sm'
+                                        <li key={point}
+                                            className='pl-1 text-sm font-normal text-black-500/50'
                                         >
                                             {point}
                                         </li>
@@ -95,9 +96,9 @@ const About = () => {
                     </VerticalTimeline>
                 </div>
             </div>
-            <hr className='border-slate-200'/>
+            <hr className='border-slate-200' />
 
-            <CTA />
+            <Cta />
         </section>
     )
 }

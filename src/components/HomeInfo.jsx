c8aaf723788f407e9fdaaef035e72eed71361752
VU <vu@VUs-MacBook-Pro-2.local>
2025-03-02 23:24:58 +0700

    update imaginify to porfolio

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -5,12 +5,12 @@ import { arrow } from '../assets/icons'
 
 const InfoBox = ({ text, link, btnText }) => (
     <div className='info-box'>
-        <p className='font-medium sm:text-xl text-center'>{text}</p>
-        <Link to={link} className='neo-brutalism-white text-white neo-btn'>
+        <p className='font-medium text-center sm:text-xl'>{text}</p>
+        <Link to={link} className='text-white neo-brutalism-white neo-btn'>
             {btnText}
             <img
                 src={arrow}
-                className='w-4 h-4 object-contain'
+                className='object-contain w-4 h-4'
             />
         </Link>
     </div>
@@ -19,8 +19,8 @@ const InfoBox = ({ text, link, btnText }) => (
 
 const renderContent = {
     1: (
-        <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
-            Hi, I am <span className=' font-semibold'>BlackCat13</span> 👋
+        <h1 className='px-8 py-4 mx-5 text-center text-white sm:text-xl sm:leading-snug neo-brutalism-blue'>
+            Hi, I am <span className='font-semibold '>Mr.Vu</span> 👋
             <br />
             A Software Engineer from VietNam.
         </h1>
